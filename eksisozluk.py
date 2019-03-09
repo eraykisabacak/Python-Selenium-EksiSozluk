1c9d9d108e824e0d53ff527085a21daa94e06d31
Eray Kısabacak <nefret19981998@hotmail.com>
2019-03-09 09:57:07 +0300

    Python Selenium EksiSözlük

---
+++ eksisozluk.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+import random
+import time
+
+browser = webdriver.Firefox()
+
+
+url = "https://eksisozluk.com/mustafa-kemal-ataturk--34712?p="
+
+pageCount = 1
+
+entries = []
+entryCount = 1
+
+while pageCount <= 10:
+    randomPage = random.randint(1,1290)
+    newUrl = url + str(randomPage)
+    browser.get(newUrl)
+
+    elements = browser.find_elements_by_css_selector('.content')
+    for element in elements: 
+        entries.append(element.text)
+    
+    time.sleep(3)
+    pageCount += 1
+
+with open("entries.txt","w",encoding = "UTF-8") as file:
+    for entry in entries:
+        file.write(str(entryCount) + ".\n" + entry + "\n")
+        file.write("********************************************\n")
+        entryCount += 1
+
+browser.close()
+
+"""for entry in entries:
+    print(str(entryCount) + "**********************************")
+    print(entry)
+    entryCount += 1"""
+
+
+
+#browser.get(url)
+
+#time.sleep(5)
+
+"""elements = browser.find_elements_by_css_selector('.content')
+
+for element in elements:
+    print("**********************************");
+    print(element.text)"""
+
+
